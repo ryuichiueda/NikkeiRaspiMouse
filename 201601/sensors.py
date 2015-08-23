a01aac2df1ed0b5e50a81960827a224065967154
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-08-23 21:14:28 +0000

    Add comments

--- 201601/sensors.py
+++ 201601/sensors.py
@@ -21,16 +21,11 @@ class Sensor:
 				time.sleep(0.001)
 			
 class Buttons(Sensor):
-
 	def __init__(self):
 		Sensor.__init__(self)
 		self.__files = sorted(glob.glob("/dev/rtswitch[0-2]"))
 		self.__pushed = [False,False,False]
 		self.__values = ["1","1","1"]
-
-	def update(self):
-		self.__values = map(self._readline,self.__files)
-		self.__pushed = [ v == "0" or past for (v,past) in zip(self.__values,self.__pushed)]
 
 	def __check_pushed(self,num):
 		if self.__values[num] == "0": #now pushing
@@ -42,6 +37,14 @@ class Buttons(Sensor):
 		else:
 			return False
 
+	#デバイスファイルからのデータ読み込み
+	def update(self):
+		#現在の値の取得
+		self.__values = map(self._readline,self.__files)
+		#押されたら*_pushedが呼ばれるまでTrueを保持するリスト
+		self.__pushed = [ v == "0" or past for (v,past) in zip(self.__values,self.__pushed)]
+
+	#公開するボタンの状態取得メソッド
 	def front_pushed(self):    return self.__check_pushed(0)
 	def center_pushed(self):   return self.__check_pushed(1)
 	def back_pushed(self):     return self.__check_pushed(2)
