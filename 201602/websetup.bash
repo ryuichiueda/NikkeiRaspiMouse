018074881f2d1e3409f05099da6e17c25aa48b86
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-09-25 05:11:29 +0000

    Change some configulations for websetup

--- 201602/websetup.bash
+++ 201602/websetup.bash
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+#このファイルのあるディレクトリで実行のこと（手抜き）
 
 #スティッキービットの解除
 sudo chmod o-t /run/shm/
@@ -12,6 +12,8 @@ sudo rsync -av ./web/ /var/www/
 
 #apacheの設定と再起動
 sudo cp ./raspimouse.apache2.conf /etc/apache2/sites-available/
-sudo rm -f /etc/apache2/sites-enabled/*default*
+sudo rm -f /etc/apache2/sites-enabled/*
 sudo ln -s /etc/apache2/sites-available/raspimouse.apache2.conf /etc/apache2/sites-enabled/raspimouse.conf
+
+sudo a2enmod cgi
 sudo service apache2 restart
