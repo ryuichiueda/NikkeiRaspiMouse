4c164a4060f468b30bddea812a6f874710c97b92
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-11-13 15:00:43 +0900

    Change the priority

--- 201601/sys/robot.sh
+++ 201601/sys/robot.sh
@@ -2,8 +2,8 @@
 exec 2> /run/shm/init.log
 
 ### set the driver ###
-insmod /home/pi/RaspberryPiMouse/lib/Pi?B+/`uname -r`/rtmouse.ko ||
-insmod /home/pi/RaspberryPiMouse/src/drivers/rtmouse.ko
+insmod /home/pi/RaspberryPiMouse/src/drivers/rtmouse.ko ||
+insmod /home/pi/RaspberryPiMouse/lib/Pi?B+/`uname -r`/rtmouse.ko 
 sleep 1
 chmod 666 /dev/rt*
 
