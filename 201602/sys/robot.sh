24082c9c5573cb8b27ac4383e26b10b34189bcdb
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-11-21 22:10:41 +0000

    Recover the setting of sticky bit

--- 201602/sys/robot.sh
+++ 201602/sys/robot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -evx
 exec 2> /run/shm/init.log
 
+### remove sticky bit ###
+sudo chmod o-t /run/shm/
+
 ### set the driver ###
 insmod /home/pi/RaspberryPiMouse/src/drivers/rtmouse.ko ||
 insmod /home/pi/RaspberryPiMouse/lib/Pi?B+/`uname -r`/rtmouse.ko 
