2e5d077fb72c27b95b378cf5db30a290e4b4d6b9
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-11-21 22:03:09 +0000

    Copy robot.sh from 201601 to 201602

--- 201602/sys/robot.sh
+++ 201602/sys/robot.sh
@@ -3,12 +3,9 @@ exec 2> /run/shm/init.log
 
 ### set the driver ###
 insmod /home/pi/RaspberryPiMouse/src/drivers/rtmouse.ko ||
-insmod /home/pi/RaspberryPiMouse/lib/Pi2B+/$(uname -r)/rtmouse.ko
+insmod /home/pi/RaspberryPiMouse/lib/Pi?B+/`uname -r`/rtmouse.ko 
 sleep 1
 chmod 666 /dev/rt*
 
 ### motor power off ###
 echo 0 > /dev/rtmotoren0
-
-###スティッキービットの解除###
-sudo chmod o-t /run/shm/
