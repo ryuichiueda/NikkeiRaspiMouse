06ffee8e99971d2a5fa4159a7523de31a140d574
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-02-26 11:26:59 +0000

    Finish writing controller part

--- 201606/run.bash
+++ 201606/run.bash
@@ -5,9 +5,14 @@ fin () {
 	exit 1
 }
 
-trap fin 1
+trap fin 1 2 3 15
 
+### remove sticky bit ###
+sudo chmod o-t /run/shm/
+
+### run julius ###
 ( cd ~/dictation-kit/ && julius -C main.jconf -input mic -C am-gmm.jconf -n 1 -module ) &
 
+### run controller ###
 ./voice_control.py
 
--- 201606/voice_control.py
+++ 201606/voice_control.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python
 # vim:fileencoding=utf-8
 
-import socket
+import socket, os, time
 
 def get_line(s):
-	line = ""
-	while True:
-		v = s.recv(1)
-		line += v
-		if v == '\n':
-			return line
+    line = ""
+    while True:
+        v = s.recv(1)
+        line += v
+        if v == '\n':
+            return line
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost",10500))
+def connect():
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(("localhost",10500))
+            return s
+        except:
+            time.sleep(1.0)
 
 def put_op(command):
-	filename = "/run/shm/op"
-	tmp_filename = filename + ".tmp"
-	with open(tmp_filename,"w") as f:
-		f.write(command + '\n')
+    filename = "/run/shm/op"
+    tmp_filename = filename + ".tmp"
+    with open(tmp_filename,"w") as f:
+        f.write(command + '\n')
 
-	os.chmod(tmp_filename,0666)
-	os.rename(tmp_filename,filename)
+    os.chmod(tmp_filename,0666)
+    os.rename(tmp_filename,filename)
 
-while True:
-	line = get_line(s)
-	if '前' in line: put_op('fw')
-	if '左' in line: put_op('left')
-	if '右' in line: put_op('right')
+if __name__ == '__main__':
+    s = connect()
+    while True:
+        line = get_line(s)
+        if '前' in line: put_op('fw')
+        if '左' in line: put_op('left')
+        if '右' in line: put_op('right')
